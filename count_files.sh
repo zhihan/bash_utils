db9f6b284a401f0fec9b9827a73a0d421ca1f5e5
Zhi Han <zhi.han@gmail.com>
2016-01-04 15:27:06 -0500

    Initial commit

---
+++ count_files.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# A small utilty to count the number of files with certain extension.
+#
+
+function extension() {
+  local filename="${1}"
+  echo "${filename##*.}"
+}
+
+
+function main() {
+  declare -A counts
+  local ext=''
+
+  for file in "$@"
+  do
+    if [[ -f "${file}" ]]; then
+      ext="$(extension ${file})"
+      if [[ -z "${counts[$ext]+tested}" ]]; then
+        let counts["$ext"]=1
+      else
+        let counts["$ext"]=${counts["$ext"]}+1
+      fi
+    fi 
+  done
+
+  for ext in "${!counts[@]}"
+  do
+    echo "${ext}: ${counts[$ext]}"
+  done
+}
+
+if (($# > 0)); then
+  # If a list of files is given, use it to count files.
+  main $@
+else
+  # Default option
+  # Invoke the find command to list all files in
+  files=$(find . -not -path "./.git/*") 
+  main $files
+fi
---
+++ test_me.sh
@@ -0,0 +1,3 @@
+echo "All arguments $@"
+echo "First $1"
+echo "Prog $0"
