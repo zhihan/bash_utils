1d995b3bb58b118770e79726fe03b547976776e4
Zhi Han <zhi.han@gmail.com>
2016-01-08 14:11:35 -0500

    More fixes

--- cleanup.sh
+++ cleanup.sh
@@ -6,8 +6,8 @@
 function remove_files() {
   # Remove all files matched by a pattern
   local pattern="$1"
-  files=( $(find . -name \*.pyc) )
-
+  files=( $(find . -name ${pattern}) )
+  echo "${pattern}: ${files}"
   if [[ ! -z "${files}" ]]; then
       for f in ${files[*]}; do
 	  rm -rf "$f"
@@ -18,9 +18,9 @@ function remove_files() {
 
 function main() {
   # Main function
-  declare -a temp_pat=(\\*~ \\*.pyc)
+  declare -a temp_pat=(\*~ \*.pyc)
   for pat in ${temp_pat[*]}; do
-      remove_files "${pat}"
+      (remove_files "${pat}")
   done
 }
 
