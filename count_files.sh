97e9451c79247835daa6f9089a175f8fa011b7e7
zhihan <zhi.han@gmail.com>
2016-01-11 22:56:42 -0500

    Added file types. Added more filters

--- count_files.sh
+++ count_files.sh
@@ -16,26 +16,39 @@ function file_length() {
 function main() {
   declare -A counts
   declare -A line_counts
+
+  declare -A known_types
+  known_types[py]="Python source"
+  known_types[sh]="Bash script"
+  known_types[md]="Markdown file"
+  known_types[java]="Java source"
+  known_types[groovy]="Groovy source"
+  known_types[scala]="Scala source"
+  known_types[m]="Matlab source"
+  
   local ext=''
 
   for file in "$@"
   do
     if [[ -f "${file}" ]]; then
       ext=$(extension ${file})
-      declare -i n=$(file_length ${file})
-      if [[ -z "${counts[$ext]+tested}" ]]; then
-        let counts["$ext"]=1
-        let line_counts["$ext"]=$n
-      else
-        let counts["$ext"]=${counts["$ext"]}+1
-        let line_counts["$ext"]=${line_counts["$ext"]}+$n
+      if [[ ! -z "${known_types[$ext]}" ]]; then  
+        declare -i n=$(file_length ${file})
+        if [[ -z "${counts[$ext]+tested}" ]]; then
+          let counts["$ext"]=1
+          let line_counts["$ext"]=$n
+        else
+          let counts["$ext"]=${counts["$ext"]}+1
+          let line_counts["$ext"]=${line_counts["$ext"]}+$n
+        fi
       fi
     fi 
   done
 
   for ext in "${!counts[@]}"
   do
-    echo "${ext}: ${counts[$ext]} file(s), ${line_counts[$ext]} lines."
+    echo -e "\033[32m${known_types[$ext]}:\033[m"
+    echo "${counts[$ext]} file(s), ${line_counts[$ext]} lines."
   done
 }
 
@@ -45,6 +58,8 @@ if (($# > 0)); then
 else
   # Default option
   # Invoke the find command to list all files in
-  files=$(find . -not -path "./.git/*") 
+    files=$(find . \
+                 -not -path "*.git/*" \
+                 -not -path "*/target/*") 
   main $files
 fi
